61f198a6d9729dcb991b8329b1896a26ec9e7de2
Willie <wcohen64@gmail.com>
2018-04-15 18:29:29 -0400

    create driver class and fix article constructor

---
+++ src/parallelBOAT/Driver.java
@@ -0,0 +1,44 @@
+package parallelBOAT;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Driver {
+
+    public static void main(String [] args) {
+
+        Article[] rawData = new Article[39797];
+        BufferedReader br = null;
+        String file = "./resources/OnlineNewsPopularity.csv";
+        String line = "";
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+            int i = -1;
+            while ((line = br.readLine()) != null) {
+                if(i == -1) {
+                    i++;
+                    continue;
+                }
+                rawData[i] = new Article(line.split(", "));
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+}
